539e496c8d4cc07026e90e268652ec23a5c9d2da
Jujuyeh <paul.hodgetts@sysdig.com>
2025-11-07 16:49:25 +0100

    chore: Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,30 @@
-export default [
+// Use CommonJS for the flat config file so Node doesn't require "type": "module"
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
   {
-    files: ["**/*.ts"],
-    languageOptions: { parserOptions: { ecmaVersion: "latest", sourceType: "module" } },
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+        // If you later enable type-aware rules, add:
+        // project: ['./tsconfig.build.json'],
+        // tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
-      "no-console": "off",
-      "prefer-const": "error"
-    }
-  }
+      'no-console': 'off',
+      'prefer-const': 'error',
+
+      // Basic TS linting (add more as you like)
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports' }],
+    },
+  },
 ];
